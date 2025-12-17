52ba9faef81b45f826ad09610f70c4f2abd83a57
Mike Kovacina <mx7343@gmail.com>
2025-12-17 11:15:18 -0500

    Update App.cs

--- projects/wordleizer/src/App.cs
+++ projects/wordleizer/src/App.cs
@@ -4,34 +4,55 @@ Console.WriteLine("Hello, World!");
 var stream = File.OpenRead(@"data\dwyl\words_alpha.txt");
 using var reader = new StreamReader(stream);
 
-readonly var root = new Node { Value = ' ' };
-
+var root = new Node { Value = ' ' };
+int count = 0;
+int inputWordCount = 0;
 while (!reader.EndOfStream)
 {
     var line = reader.ReadLine();
-    if (line.Length == 5)
+
+    if (line == null) continue;
+
+    if (line.Length != 5) continue;
+    //Console.WriteLine(line);
+
+    inputWordCount++;   
+
+    var tracer = root;
+
+    foreach (var c in line)
     {
-        Console.WriteLine(line);
-
-        var tracer = root;
-
-        foreach(var c in line)
+        tracer.ChildMap.TryGetValue(c, out var childNode);
+        if (childNode == null)
         {
-            tracer.ChildMap.TryGetValue(c, out var childNode);
-            if (childNode == null)
-            {
-                childNode = new Node { Value = c, Count = 0 };
-                tracer.ChildMap[c] = childNode;
-            }
-            childNode.Count += 1;
-            tracer = tracer.ChildMap[c];            
+            count++;
+            childNode = new Node { Value = c, Count = 0 };
+            tracer.ChildMap[c] = childNode;
         }
+        childNode.Count += 1;
+        tracer = childNode; // Use the non-null reference directly
     }
 }
+
+var word = root;
+while (word != null)
+{
+    if (word.ChildMap.Count == 0) break;        
+
+    var max = word.ChildMap.MaxBy(kv => kv.Value.Count);
+    Console.WriteLine(max.Key + " " + max.Value.Count);
+    word = max.Value;
+}
+
+var total = root.ChildMap.Values.Sum(n1 => n1.Count);
+Console.WriteLine($"Total words: {total}"); 
+
+Console.WriteLine($"Input words: {inputWordCount}");
+Console.WriteLine($"Total unique nodes: {count}");
 
 class Node
 {
     public char Value { get; set; }
     public int Count { get; set; } = 0;
     public IDictionary<char, Node> ChildMap { get; set; } = new Dictionary<char, Node>();
-}   
+}
